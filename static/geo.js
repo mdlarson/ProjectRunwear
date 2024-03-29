d5187b25ebaf1199a69689e8be4ffaaef113af42
Mark Larson <markdlarson@hey.com>
2024-03-29 12:27:19 -0400

    Add fetchWeather() function for weather.gov forecasts

--- static/geo.js
+++ static/geo.js
@@ -7,12 +7,47 @@ function getLocation() {
     }
 }
 
+// fetch weather
+function fetchWeather(latitude, longitude) {
+    const endpoint = `https://api.weather.gov/points/${latitude},${longitude}`;
+    fetch(endpoint)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // displaying forecast URL
+            console.log(data);
+            const forecastUrl = data.properties.forecast;
+            document.getElementById('weatherOutput').innerHTML = `Forecast from <a href="${forecastUrl}">${forecastUrl}</a>`;
+
+            // fetch forecast data
+            fetch(forecastUrl)
+                .then(response => response.json())
+                .then(forecastData => {
+                    console.log(forecastData);
+                    //process further from here
+                })
+                .catch(error => console.error('Error fetching forecast: ', error));
+        })
+        .catch(error => {
+            console.error('Error fetching weather: ', error);
+            document.getElementById('weatherOutput').innerHTML = "Failed to fetch weather data.";
+        });
+}
+
+
 // show position
 function showPosition(position) {
     console.log(position.coords.latitude + ", " + position.coords.longitude);
 
     // display in output div
     document.getElementById('output').innerHTML = position.coords.latitude + ", " + position.coords.longitude;
+
+    // with location data, fetch weather
+    fetchWeather(position.coords.latitude, position.coords.longitude);
 
     // also send to Flask server
     fetch('/location', {
