6cb3a2dd38b8d558aadfd6b9a9a2a8accc5f5531
Mark Larson <markdlarson@hey.com>
2024-04-01 17:06:51 -0400

    Draft get_clothing_recommendation() and get_conditions() functions
    - create clothingRecommendationMatrix for outfits
    - create clothing_images placeholder array

--- app.py
+++ app.py
@@ -13,3 +13,59 @@ def index():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@app.route("/getClothing")
+def get_clothing_recommendation(temp, windSpeed):
+    recommendations = []
+
+    conditions = get_conditions(windSpeed)
+
+    return (clothing_images[item] for item in clothingRecommendationMatrix[temp][conditions])
+
+
+def get_conditions(windSpeed):
+    if windSpeed > 10:
+        conditions = 'windy'
+    else:
+        conditions = 'calm'
+    return conditions
+
+
+# Clothing References
+clothing_images = ['a', 'b', 'c']
+
+clothingRecommendationMatrix = {
+    '55': {
+        'calm': ["shirt", "shorts", "cap"],
+        'windy': ["longsleeve", "shorts", "cap"]
+    },
+    '50': {
+        'calm': ["longsleeve", "shorts", "cap"],
+        'windy': ["shirt", "longsleeve", "shorts", "cap"]
+    },
+    '45': {
+        'calm': ["shirt", "longsleeve", "shorts", "cap"],
+        'windy': ["shirt", "fleece", "shorts", "cap", "gloves"]
+    },
+    '40': {
+        'calm': ["shirt", "fleece", "shorts", "cap", "gloves"],
+        'windy': ["shirt", "fleece", "jacket", "lightTights", "beanie", "gloves"]
+    },
+    '35': {
+        'calm': ["shirt", "fleece", "jacket", "tights", "beanie", "gloves"],
+        'windy': ["longsleeve", "fleece", "jacket", "shorts", "tights", "beanie", "gloves"]
+    },
+    '30': {
+        'calm': ["longsleeve", "fleece", "jacket", "shorts", "tights", "beanie", "gloves"],
+        'windy': ["longsleeve", "fleece", "jacket", "tights", "joggers", "beanie", "gloves"]
+    },
+    '25': {
+        'calm': ["longsleeve", "fleece", "jacket", "fleeceTights", "joggers", "beanie", "gloves"],
+        'windy': ["longsleeve", "fleece", "jacket", "fleeceTights", "joggers", "beanie", "gloves", "scarf"]
+    },
+    '20': {
+        'calm': ["longsleeve", "fleece", "jacket", "tights", "joggers", "beanie", "gloves", "scarf"],
+        'windy': ["shirt", "longsleeve", "fleece", "jacket", "tights", "joggers", "beanie", "gloves", "scarf"]
+    }
+}
